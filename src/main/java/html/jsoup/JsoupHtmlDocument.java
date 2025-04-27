d9e12c5bb1b07df1a1dcf0635a1df80142dc1a8d
JL <115737916+yikesboy@users.noreply.github.com>
2025-04-27 17:07:38 +0200

    feat: created concrete jsoup impls for HtmlElement and HtmlDocument

---
+++ src/main/java/html/jsoup/JsoupHtmlDocument.java
@@ -0,0 +1,34 @@
+package html.jsoup;
+
+import html.HtmlDocument;
+import html.HtmlElement;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsoupHtmlDocument implements HtmlDocument {
+    private final Document document;
+
+    public JsoupHtmlDocument(Document document) {
+        this.document = document;
+    }
+
+    @Override
+    public List<HtmlElement> select(String cssSelector) {
+        Elements elements = document.select(cssSelector);
+        List<HtmlElement> htmlElements = new ArrayList<>();
+
+        for (Element element : elements) {
+            htmlElements.add(new JsoupHtmlElement(element));
+        }
+        return htmlElements;
+    }
+
+    @Override
+    public String getTitle() {
+        return document.title();
+    }
+}
