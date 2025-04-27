8b295fdac946d19ca605d95262a28c07c4f8aaea
JL <115737916+yikesboy@users.noreply.github.com>
2025-04-27 12:13:14 +0200

    feat: url utility

---
+++ src/main/java/org/example/util/UrlUtil.java
@@ -0,0 +1,32 @@
+package org.example.util;
+
+import java.net.URL;
+import java.util.Set;
+
+public class UrlUtil {
+    public static boolean isAllowedDomain(URL url, Set<String> allowedDomains) {
+        String host = url.getHost();
+
+        if (allowedDomains.contains(host)) {
+            return true;
+        }
+
+        for (String domain : allowedDomains) {
+            if (host.endsWith("." + domain)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static String normalizeUrl(URL url) {
+        String urlStr = url.toString();
+
+        if (urlStr.endsWith("/")) {
+            urlStr = urlStr.substring(0, urlStr.length() - 1);
+        }
+
+        return urlStr.toLowerCase();
+    }
+}
