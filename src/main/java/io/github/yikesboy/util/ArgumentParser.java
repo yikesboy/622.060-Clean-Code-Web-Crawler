eac8450d4450fa725dedeb998a927951980fde0a
JL <115737916+yikesboy@users.noreply.github.com>
2025-04-28 16:53:40 +0200

    fix: small whitespace fix in ArgumentParser

--- src/main/java/io/github/yikesboy/util/ArgumentParser.java
+++ src/main/java/io/github/yikesboy/util/ArgumentParser.java
@@ -43,7 +43,7 @@ public class ArgumentParser implements ArgumentParserInterface {
     }
 
     private Set<String> parseDomains(String domainArgument) {
-        String[] domainCandidates = domainArgument.split(",");
+        String[] domainCandidates = domainArgument.trim().split(",");
         Set<String> domains = new HashSet<>(List.of(domainCandidates));
         domains.removeAll(Arrays.asList("", null));
 
