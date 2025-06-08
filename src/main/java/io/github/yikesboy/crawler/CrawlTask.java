11981f4a0284ff3f494dc1125dccd9b699dc5552
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-08 18:05:54 +0200

    refactor: moved CrawlTask to crawler module

--- src/main/java/io/github/yikesboy/crawler/CrawlTask.java
+++ src/main/java/io/github/yikesboy/crawler/CrawlTask.java
@@ -1,6 +1,8 @@
-package io.github.yikesboy.models;
+package io.github.yikesboy.crawler;
 
 import io.github.yikesboy.config.CrawlConfig;
+import io.github.yikesboy.models.CrawlError;
+import io.github.yikesboy.models.WebPage;
 import io.github.yikesboy.parser.PageParserInterface;
 import io.github.yikesboy.util.UrlUtil;
 
