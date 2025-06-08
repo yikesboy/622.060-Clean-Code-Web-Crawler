0665093934506530e66be3b43edefe58e22b0fef
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-08 10:46:52 +0200

    refactor: switched CopyOnWriteArray to ConcurrentLinkedQueue

--- src/main/java/io/github/yikesboy/crawler/WebCrawlerService.java
+++ src/main/java/io/github/yikesboy/crawler/WebCrawlerService.java
@@ -7,24 +7,21 @@ import io.github.yikesboy.models.CrawlTask;
 import io.github.yikesboy.models.WebPage;
 import io.github.yikesboy.parser.PageParserInterface;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ForkJoinPool;
 
 public class WebCrawlerService implements WebCrawlerServiceInterface {
     private final PageParserInterface parser;
     private final Set<String> visitedUrls;
-    private final List<CrawlError> errors;
+    private final Queue<CrawlError> errors;
     private final ForkJoinPool forkJoinPool;
 
     public WebCrawlerService(PageParserInterface parser) {
         this.parser = parser;
         this.visitedUrls = Collections.newSetFromMap(new ConcurrentHashMap<>());
-        this.errors = new CopyOnWriteArrayList<>();
+        this.errors = new ConcurrentLinkedQueue<>();
         this.forkJoinPool = new ForkJoinPool(Math.max(2, Runtime.getRuntime().availableProcessors() / 2));
     }
 
